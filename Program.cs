5b4fa13b7de5d256e3293c100a985e96ded7b929
ofiris <ofir.shapira@mail.huji.ac.il>
2016-10-04 15:39:17 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NLog;
+using NLog.Targets;
+using NLog.Config;
+
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var config = new LoggingConfiguration();
+
+            var consoleTarget = new ColoredConsoleTarget();
+            config.AddTarget("console", consoleTarget);
+            
+            consoleTarget.Layout = @"${date:format=HH\:mm\:ss} ${logger} ${message}";
+
+            var rule1 = new LoggingRule("*", LogLevel.Debug, consoleTarget);
+            config.LoggingRules.Add(rule1);
+
+            LogManager.Configuration = config;
+
+            // Example usage
+            Logger logger = LogManager.GetLogger("Example");
+            logger.Trace("trace log message");
+            logger.Debug("debug log message");
+            logger.Info("info log message");
+            logger.Error("error log message");
+
+        }
+    }
+}
